9763175734cae5875d0b4f229955d377ab3c6c0b
krankos <bessaadkhalil@gmail.com>
2023-04-15 13:47:41 +0100

    created kafka producer. topic: btc-price

--- serveur-btc/index.js
+++ serveur-btc/index.js
@@ -1,7 +1,15 @@
 console.log("hello world")
 require('dotenv').config();
+const { Kafka } = require('kafkajs')
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092']
+})
 let apiKey = process.env.CC_API_KEY;
 const WebSocket = require('ws');
+const producer = kafka.producer()
+producer.disconnect()
+producer.connect()
 const ccStreamer = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + apiKey);
  ccStreamer.on('open', function open() {
     var subRequest = {
@@ -24,14 +32,16 @@ ccStreamer.on('message', function incoming(data) {
     var message = JSON.parse(data);
     if (message.TYPE && message.TYPE === "2") {
         if (message.PRICE) {
+            producer.send({
+                topic: 'btc-price',
+                messages: [
+                    { value: message.PRICE.toString() },
+                ],
+            })
             console.log("Binance BTC/USD: " + message.PRICE);
         }
     }
 
     
 });
-// while(true) {
-//     ccStreamer.onmessage = (message) => {
-//         console.log(message.data);
-//     }
-// }
+
