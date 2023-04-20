23ede5ec16162eb40474ac450630c46328152ec6
malouu <mmlkfehri025@gmail.com>
2023-04-20 23:56:51 +0100

    trying next server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,31 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Kafka } from 'kafkajs'
 
+
+function App() {
+
+  const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092']
+})
+async function run() {
+  console.log('hello')
+const consumer = kafka.consumer()
+
+await consumer.connect()
+await consumer.subscribe({ topic: 'btc-price', fromBeginning: true })
+
+await consumer.run({
+  eachMessage: async ({ topic, partition, message }) => {
+    console.log({
+      value: message.value.toString(),
+    })
+
+  },
+})
+  }
+ 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        hello
+      </h1>
+       
     </div>
   )
 }
