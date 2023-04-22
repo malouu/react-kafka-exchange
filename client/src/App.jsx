7b20db8288e33dfc60f944bb0e9a90df2908aaa8
malouu <mmlkfehri025@gmail.com>
2023-04-22 10:06:32 +0100

    tring to establish websocket between consumer server and the client app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import WebSocket from 'websocket'
+
 import './App.css'
 
-import { Kafka } from 'kafkajs'
+//import { Kafka } from 'kafkajs'
 
-
+//function webSocketInvoke() {
+ 
+  
+  
 function App() {
+  if (WebSocket in window) {
+    console.log('WebSocket is supported by your Browser!');
+    var ws = new WebSocket('ws://localhost:8080/”,”echo-protocol');
+    
+    ws.onopen = function() {
+    console.log('Connection created');
+    };
+    
+    ws.onmessage = function (evt) { 
+    var received_msg = evt.data;
+    console.log(received_msg );
+    };
+    
+    ws.onclose = function() { 
+    console.log('Connection closed'); 
+    };
+    } else {
+    alert('WebSocket NOT supported by your Browser!');
+    }
+    
+  //{webSocketInvoke()};
 
-  const kafka = new Kafka({
+ /* const kafka = new Kafka({
     clientId: 'my-app',
     brokers: ['localhost:9092']
 })
@@ -27,7 +53,8 @@ await consumer.run({
 
   },
 })
-  }
+  }*/
+  
  
   return (
     <div className="App">
--- consumer-btc/index.js
+++ consumer-btc/index.js
@@ -1,21 +1,68 @@
-const {Kafka} = require('kafkajs')
+//require kafka
+const { Kafka } = require('kafkajs')
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
 const kafka = new Kafka({
     clientId: 'my-app',
     brokers: ['localhost:9092']
 })
 
 const consumer = kafka.consumer({groupId: 'test-group'})
-const run = async () => {
-    await consumer.connect()
-    await consumer.subscribe({topic: 'btc-price', fromBeginning: true})
-    await consumer.run({
-        eachMessage: async ({topic, partition, message}) => {
-            console.log({
-                value: message.value.toString(),
-            })
 
-        },
-    })
-}
+var server = http.createServer(function(request, response) {
+    console.log(' Request recieved : ' + request.url);
+    response.writeHead(404);
+    response.end();
+   });
+   server.listen(8080, function() {
+    console.log('Listening on port : 8080');
+   });
+    
+   WebSocketServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false,
+    
+   });
+    
+   function iSOriginAllowed(origin) {
+    return true;
+   }
+    
+   WebSocketServer.on('request', async function(request) {
+    if (!iSOriginAllowed(request.origin)) {
+    request.reject();
+    onsole.log('malou');
+    console.log(' Connection from : ' + request.origin + ' rejected.');
+    return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    onsole.log('khalil');
+    console.log(' Connection accepted : ' + request.origin);
+    connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+    console.log('Received Message: ' + message.value.toString());
+    }
+    });
 
-run().catch(console.error)
+    //const run = async () => {
+        await consumer.connect()
+        await consumer.subscribe({topic: 'btc-price', fromBeginning: true})
+        await consumer.run({
+            eachMessage: async ({topic, partition, message}) => {
+                connection.sendUTF(message.value.toString());
+                console.log({
+                    value: message.value.toString(),
+                })
+    
+            },
+        })
+    //}
+    connection.on('close', function(reasonCode, description) {
+        console.log('Connection ' + connection.remoteAddress + ' disconnected.');
+        });
+       });
+
+
+      // run().catch(console.error)
