78d9423c891688caefba9af372cd2ed770d2d092
malouu <mmlkfehri025@gmail.com>
2023-04-26 18:53:12 +0100

    Chart line BTC/second using chartjs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,90 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import WebSocket from 'websocket'
-// import WebSocket from 'ws'
 
 import './App.css'
-
-//import { Kafka } from 'kafkajs'
-
-//function webSocketInvoke() {
- 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
   
   
 function App() {
-  // if (WebSocket in window) {
-  //   console.log('WebSocket is supported by your Browser!');
-  //   var ws = new WebSocket('ws://localhost:8080/”,”echo-protocol');
-    
-  //   ws.onopen = function() {
-  //   console.log('Connection created');
-  //   };
-    
-  //   ws.onmessage = function (evt) { 
-  //   var received_msg = evt.data;
-  //   console.log(received_msg );
-  //   };
-    
-  //   ws.onclose = function() { 
-  //   console.log('Connection closed'); 
-  //   };
-  //   } else {
-  //   alert('WebSocket NOT supported by your Browser!');
-  //   }
-    
-  //{webSocketInvoke()};
-
- /* const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['localhost:9092']
-})
-async function run() {
-  console.log('hello')
-const consumer = kafka.consumer()
-
-await consumer.connect()
-await consumer.subscribe({ topic: 'btc-price', fromBeginning: true })
-
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log({
-      value: message.value.toString(),
-    })
-
-  },
-})
-  }*/
-
   const [price, setPrice] = useState(0)
   const [ws, setWs] = useState(null)
-  const [color, setColor] = useState('white')
-
+  const [color, setColor] = useState('')
+  const [data,setData] = useState([]);
+  const [seconds, setSeconds] = useState([]);
+  const deadline = "December, 31, 2023";
+  const time = Date.parse(deadline) - Date.now();
   
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      //si la taille de second est superiur a 20 on supprime le premier element
+      if (seconds.length > 10) {
+        seconds.shift()
+      }
+      setSeconds([...seconds,Math.floor((time / 1000) % 60)]);
+    }, 1000); 
+    return () => clearInterval(interval);
+  }, [seconds]);
+
+
+  console.log(seconds);
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  
+  const options = {
+    
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' 
+      },
+      title: {
+        display: true,
+        text: 'Chart Line',
+      },
+    },
+  };
+  
+ /// const labels = [1,2,3,4,5,6,7,8,9];
+  
+  const datavisulise = {
+    labels: seconds,
+    datasets: [
+      {
+        label: 'BTC/second',
+        data: data,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        //hide points
+        pointRadius: 0,
+        //largest hight and width
+        
+
+
+        
+
+      },
+      
+    ],
+  };
 
   if (!ws) {
     const ws = new WebSocket('ws://localhost:8080/'); 
@@ -81,10 +105,16 @@ await consumer.run({
         setColor('red')
       }
       else {
-        setColor('white')
+        setColor('black')
       }
       setPrice(received_msg)
+      
+      if (data.length > 10) {
+        data.shift()
+      }
+      setData([...data,received_msg])
       };
+     
   }
 
 
@@ -96,6 +126,7 @@ await consumer.run({
         BTC Price
       </h1>
       <h2 className={color}>Price: {price}</h2>
+      <Line options={options} data={datavisulise} />;
        
     </div>
   )
